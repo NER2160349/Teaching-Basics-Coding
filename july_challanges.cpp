a5efb1d4c2a8923d2a088dcde063c7f7e73724e9
Nereida <89960249+NER2160349@users.noreply.github.com>
2024-07-01 22:01:53 +0000

    Added the challanges done for June, and went over two challanges today for July

---
+++ july_challanges.cpp
@@ -0,0 +1,30 @@
+// Here is what we have done for the month of July
+
+#include <iostream>
+using namespace std;
+
+int main(){
+/*Challenge 5: Calculate Sum of Numbers fom 1 to 10
+int sum;
+for(int i = 0; i <=10; i++){
+    sum+= i;
+}
+cout << " The Sum of Numbers fom 1 to 10: " << sum << endl;*/
+
+/*Challenge 6: Calculate 10!
+int factorial = 1;
+cout << "10! = ";
+for(int i = 10; i >=1; i--){
+    factorial *= i;
+    if(i != 1){
+    cout << i << " x ";
+    }
+    else{
+    cout << i << " = ";
+    }
+}
+cout << factorial << endl;*/
+
+return 0;
+}
+
---
+++ june_challanges.cpp
@@ -0,0 +1,43 @@
+// Here is what we have done for the month of June
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    /*Challenge 1: Print Numbers from 1 to 10
+    cout <<"Numbers from 1 - 10: ";
+    for(int i = 1; i <= 10; i++){
+        cout << i << " ";
+    }*/
+
+   /*Challenge 2: Print Odd Numbers less than 100
+   cout <<"Print Odd Numbers less than 100: ";
+   for(int i = 0; i <= 100; i++){
+    if(i % 2 != 0){
+        cout << i << ", ";
+    }
+   }*/
+
+   /*Challenge 3: Print Multiplication table with 7
+   cout << "Print Multiplication table with 7: " << endl;
+   for(int i = 0; i <= 12; i++){
+    cout << "7 x " << i << " = " << 7*i << endl; 
+   }*/
+
+   /*Challenge 4: Print All Multiplication Tables (1-10)
+   cout << "Multiplication Tables (1-10)" << endl;
+   for(int i = 0; i <=12; i++){
+    cout << i << "s table: " << endl;
+    cout << "-------------" << endl;
+    for(int j = 0; j <=10; j++){
+        if(j != 10){
+        cout << j << " x " << i << " = " << j*i << endl;
+        }
+        else {
+        cout << j << " x " << i << " = " << j*i << endl << endl;
+        }
+    }
+   }*/
+    return 0;
+}
